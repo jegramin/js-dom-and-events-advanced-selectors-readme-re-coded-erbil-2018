dbbe8a77727c741de91958efe59280c007169748
Mohammed Ameen <jegroamin@gmail.com>
2018-09-11 23:03:00 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -11,3 +11,6 @@ function ninjaBabySelector(){
 function divSelector(){
   return document.getElementsByClassName("pics")
 }
+function divSelector(){
+  return document.getElementsByClassName("pics")
+}
